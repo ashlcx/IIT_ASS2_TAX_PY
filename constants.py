f520b29e9ea5901522b830911db5f64d34ad14d8
Ash Hines <ashhines95@gmail.com>
2021-05-01 14:53:28 +1000

    Add Increment Constants

--- constants.py
+++ constants.py
@@ -3,5 +3,11 @@
 C_VERSION = "1.0.0"
 C_FY = ("1920", "2021", "2121")
 C_TAXFILE = 'taxrates.json'
+C_INCREMENT_HOURLY = "Hourly"
+C_INCREMENT_DAILY = "Daily"
+C_INCREMENT_WEEKLY = "Weekly"
+C_INCREMENT_FORTNIGHTLY = "Fortnightly"
+C_INCREMENT_MONTHLY = "Monthly"
+C_INCREMENT_ANNUALLY = "Annually"
 
 ###
