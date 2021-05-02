b00e196f81f234ff47d0779e9a98b963da60b07d
Ash Hines <ashhines95@gmail.com>
2021-05-02 15:16:57 +1000

    Update for stupid requirements

--- app.py
+++ app.py
@@ -13,25 +13,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import sys
 
+## Imports
+import sys
 import GUI
 import CLI
-import constants as C
+import constants as CONSTS
 
+## Start of program ARGS Parser (Effectively Main() - Allows import without calling function.)
 if __name__ == "__main__":
     # Get System Args to check if GUI or CLI
     if (len(sys.argv) > 1):
+        ## Checks if application has Arguments
         # print(sys.argv[1])
         # print(sys.argv[1].lower())
         if(sys.argv[1].lower() == "cli"):
-            # Begin CLI
+            ## Call CLI
             cli = CLI.CLI()
             cli.cliMenu()
         else:
-            print("Pay Calculator {}".format(C.C_VERSION))
+            ## Print out version + Syntax for app entry
+            print("Pay Calculator {}".format(CONSTS.C_VERSION))
             print(
                 "Invalid Argument - Syntax should be 'python3 app.py cli' or 'python3 app.py'")
+            ## Kill application
             sys.exit(1)
     else:
+        ## call GUI
         GUI.start()
